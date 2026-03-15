fddb9f6ee70135764ec4de2c3b878a38066e752a
doejoe13 <doejoe13@gmail.com>
2026-03-15 11:55:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-CSV_PATH = Path("/home/csv-server/data/sklady.csv")
+CSV_PATH = Path("./data/sklady.csv")
 
 def load_csv():
     """Load CSV on each request to reflect hourly updates."""
